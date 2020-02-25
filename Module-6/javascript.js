d25f773b9143c1aa6d8acfb86cbda9fd34222a73
Jonathan Kelley <jonathankelley87@gmail.com>
2020-02-24 21:08:43 -0500

    initial commit

---
+++ Module-6/javascript.js
@@ -0,0 +1,19 @@
+document.getElementById ("button1").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "300px"
+})
+
+document.getElementById ("button2").addEventListener("click", function(){
+
+    document.getElementById("box").style.backgroundColor = "blue"
+})
+
+document.getElementById ("button3").addEventListener("click", function(){
+
+    document.getElementById("box").style.opacity = 0.3
+})
+
+document.getElementById ("button4").addEventListener("click", function(){
+
+    document.getElementById("box").style="height:150px; width:150px; background-color:orange; margin:25px"
+})
